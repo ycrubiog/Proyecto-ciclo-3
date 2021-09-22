f107690a443c0f480cc80128ed2a39053fd8469a
sronaal <88067203+sronaal@users.noreply.github.com>
2021-09-22 10:36:35 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgendarCitas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendarCitas.Models
+{
+    public class Persona
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Cedula { get; set; }
+
+        public string Nombre { get; set; }
+
+
+        public string Apellido { get; set; } 
+
+
+        public Persona(int Id,string Cedula,string Nombre,string Apellldio)
+        {
+            this.Id = Id;
+            this.Cedula = Cedula;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+        }
+
+    }
+}
