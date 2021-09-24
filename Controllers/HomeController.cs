3a6057f8b21265122d7da963a67c2a9d7000add2
sronaal <88067203+sronaal@users.noreply.github.com>
2021-09-24 18:44:39 -0500

    Actualizar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,10 +23,7 @@ namespace WebAppAgendarCitas.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
