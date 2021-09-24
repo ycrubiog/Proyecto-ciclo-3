fa89883ddfff88c351e911bca9cbee18bc4e1829
sronaal <88067203+sronaal@users.noreply.github.com>
2021-09-24 17:04:58 -0500

    Actualizado

---
+++ Models/Medico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAgendarCitas.Models
+{
+    public class Medico : Persona
+    {
+        public string numeroConsultorio { get; set; }
+    
+        public Medico(int Id,string Cedula,string Nombre,string Apellido,string numeroConsultorio) : base(Id,Cedula,Nombre,Apellido)
+        {
+            this.numeroConsultorio = numeroConsultorio;
+        }
+    }
+}
---
+++ Models/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppAgendarCitas.Models
+{
+    public class Paciente : Persona
+    {
+        public string Telefono { get; set; }
+
+        public Paciente(int Id,string Cedula,string Nombre,string Apellido,string Telefono) : base(Id,Cedula,Nombre,Apellido)
+        {
+            this.Telefono = Telefono;
+        }
+
+    }
+
+
+}
