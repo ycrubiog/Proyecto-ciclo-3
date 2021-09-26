fa537b6ff6128153a36518e3729141174f3a7642
sronaal <88067203+sronaal@users.noreply.github.com>
2021-09-26 16:45:20 -0500

    Actualizado

--- wwwroot/js/AgendarCita.js
+++ wwwroot/js/AgendarCita.js
@@ -7,4 +7,10 @@
     var nombre = formulario['inputNombre'].value;
     var apellido = formulario['inputApellido'].value;
     var telefono = formulario['inputTelefono'].value;
+
+
+    if (cedula.length < 10 | cedula.length) {
+        console.log("La longitud de la cedula debe tener 10 caracteres");
+    }
+    
 }
