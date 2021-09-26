e230426b7d4a9faf5e68181e59eff1a0f84bfdc4
ronal.sanchez00219@outlook.com <ronal.sanchez00219@outlook.com>
2021-09-25 19:14:49 -0500

    Actualizado

--- wwwroot/js/AgendarCita.js
+++ wwwroot/js/AgendarCita.js
@@ -5,4 +5,6 @@
 
     var cedula = formulario['inputCedula'].value;
     var nombre = formulario['inputNombre'].value;
+    var apellido = formulario['inputApellido'].value;
+    var telefono = formulario['inputTelefono'].value;
 }
