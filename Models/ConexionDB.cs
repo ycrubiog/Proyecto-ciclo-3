b2a770fdd74620becdbcb4c3ddd3b142b1fedef0
sronaal <88067203+sronaal@users.noreply.github.com>
2021-10-12 13:00:32 -0500

    Ok

---
+++ Models/ConexionDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebAppAgendarCitas.Models
+{
+    public class ConexionDB
+    {
+        static private string CadenConexion = "Server=Lapto-Win;DataBase=ColegioApp;Integrated Security=True";
+
+        private SqlConnection conexion = new SqlConnection(CadenConexion);
+
+        public SqlConnection abrirConexion()
+        {
+            if(conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+
+            return conexion;
+        }
+
+
+        public SqlConnection cerrarConexion()
+        {
+            if(conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+
+            return conexion;
+        }
+    }
+}
